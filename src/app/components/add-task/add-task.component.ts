6a35c986676efecea7a88d06bc2a3c6aef15b4e1
Mike Holman <softwaretech@mynetfamily.com>
2023-03-31 16:05:13 -0600

    Task Tracker init

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask = new EventEmitter<Task>();
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please enter a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+
+  // TODO: emit submit event
+}
